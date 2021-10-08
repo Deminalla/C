7484adfd9f9420605e0ac1259a293f6ba94c0d5c
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-10-08 15:38:04 +0300

    Update 1ir3uzd.c

--- 1ir3uzd.c
+++ 1ir3uzd.c
@@ -1,3 +1,5 @@
+//Įvesti sveikų skaičių seką, kurios pabaiga žymima skaičiumi 0. Išvesti narių, kurių skaitmenų suma nelyginė, skaičių. 
+//input integer numbers, the sequence should end with 0. Output the amount of times the input integers' digits' numbers added up to an unequal number
 #include <stdio.h>
 
 int main()
@@ -19,15 +21,15 @@ int main()
             laikinas_sk = skaicius;
             while (laikinas_sk!=0)
             {
-                suma = suma + laikinas_sk%10;
-                laikinas_sk = laikinas_sk/10;
+                suma = suma + laikinas_sk%10;  //the last digit of a number is added
+                laikinas_sk = laikinas_sk/10;  //the last digit is taken out of the number
             }
             if (suma%2==1)
             {
                 nelyg_nariai++;
             }
         }
-        else while (getchar()!='\n'); //pasalina visus simbolius iki tol kol ju nelieka
+        else while (getchar()!='\n'); //in case a letter or words are scanned, this cycle will get rid of all the symbols until none are left
     }
     printf("Nariu, kuriu skaitmenu suma nelygine, skaicius: %d", nelyg_nariai);
     return 0;
