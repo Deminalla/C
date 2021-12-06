4abbbf55859ca297d5aee77ff9db3c8b02241402
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-06 19:40:04 +0300

    Create new_node.h

---
+++ circular list, multiple files/additional/new_node.h
@@ -0,0 +1,9 @@
+#ifndef NEW_NODE_H
+#define NEW_NODE_H
+/* ^^ these are the include guards */
+
+/* Prototypes for the functions */
+/* Sums two ints */
+void new_node_before(int *value, int *criteria);
+
+#endif
