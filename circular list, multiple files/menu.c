d0dad976959aadeb267048548148cfe2600f17a2
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-06 19:38:41 +0300

    Delete menu.c

--- circular list, multiple files/menu.c
+++
@@ -1,9 +0,0 @@
-#include "menu.h"
-#include <stdio.h>
-int menu(int choice){
-printf("\n*********** MENU *************\n");
-        printf("1. Create circular list \n2. Display circluar list \n3. Insert element \n4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        return choice;
-}
