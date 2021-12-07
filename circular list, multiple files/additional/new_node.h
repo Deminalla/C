2db869f53a5ca1c3017e93dc6a0713ae31420935
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-07 16:40:59 +0300

    Update new_node.h

--- circular list, multiple files/additional/new_node.h
+++ circular list, multiple files/additional/new_node.h
@@ -1,4 +1,4 @@
-#ifndef NEW_NODE_H
+#ifndef NEW_NODE_H  //if a compiler comes to this file multiple times it will show an error because it will try to define many times, so we use ifndef to make sure it wasnt defined before
 #define NEW_NODE_H
 /* ^^ these are the include guards */
 
