64fd342dec2d0430d09f0ba8ce9d483c12905de6
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-08 23:36:35 +0300

    Create main.h

---
+++ circular list, multiple files/c and header/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+struct Node 
+{
+    int data;
+    struct Node *next; 
+};
+typedef struct Node Node; //update  pridėjau typedef, kaip ir sakėt, tai jau užteks rašyt tik Node vietoj struct Node
+
+void new_node_before(int *value, int *criteria);
+void read_file(FILE *list_file, char *list_filename);
+
+
+#endif
