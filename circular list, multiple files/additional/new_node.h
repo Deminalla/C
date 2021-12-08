50adb81a090ade5b6404e3ef1f5858b1417120a7
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-08 23:36:48 +0300

    Delete circular list, multiple files/additional directory

--- circular list, multiple files/additional/new_node.h
+++
@@ -1,8 +0,0 @@
-#ifndef NEW_NODE_H  //if a compiler comes to this file multiple times it will show an error because it will try to define many times, so we use ifndef to make sure it wasnt defined before
-#define NEW_NODE_H
-/* ^^ these are the include guards */
-
-/* Prototypes for the functions */
-void new_node_before(int *value, int *criteria);
-
-#endif
--- circular list, multiple files/additional/read.h
+++
@@ -1,6 +0,0 @@
-#ifndef READ_H
-#define READ_H
-#include <stdio.h>
-void read_file(FILE *list_file, char *list_filename);
-
-#endif
