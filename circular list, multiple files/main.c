4adccdcb56cd2a46f59054db7828a5b4ddef4913
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-09 00:14:18 +0300

    Update main.c

--- circular list, multiple files/main.c
+++ circular list, multiple files/main.c
@@ -127,22 +127,3 @@ void insert()
         printf("%d could not be found in the list\n", criteria);
     }
 }
-/*
-void display() // no need for parameters, because head, data, next are already global and the only new one (temp) is created inside the function
-{
-    if (head == NULL)
-        printf("List is empty\n");
-
-    else
-    {
-        printf("List elements are: \n");
-        Node *temp = head; // define a Node pointer 'temp' and initialize with head, temp as in temporary and temp doesnt work outside this else function, its like a very local variable
-        assert(temp != NULL);
-        while (temp->next != head)
-        {
-            printf("%d -> ", temp->data); // keep displaying temp → data until temp reaches the last node
-            temp = temp->next;
-        }
-        printf("%d\n", temp->data); // i could also add head -> data then it would also print the first element in the list
-    }
-}*/
