99d7a648da7b63c5a4463028a7dbb96e62d254a5
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-12-09 19:49:57 +0300

    Update create.c

--- circular list, multiple files/c and header/create.c
+++ circular list, multiple files/c and header/create.c
@@ -8,10 +8,10 @@ void create_list(FILE *list_file, Node **head, Node **tail) // This function wil
     int value;
     if ((fscanf(list_file, "%d", &value) != EOF))
     {
-        Node *new_node = (Node *)malloc(sizeof(Node)); // Create new node, we allocate memory if we will give it a value 
+        Node *new_node = (Node *)malloc(sizeof(Node)); // Create new node, we allocate memory if we will give it a value
         assert(new_node != NULL);
-        new_node->data = value;      // assign a value to the new node
-        if ((*head) == NULL)            // If list is empty, both head and tail would point to new node.
+        new_node->data = value; // assign a value to the new node
+        if ((*head) == NULL)    // If list is empty, both head and tail would point to new node.
         {
             (*head) = new_node;
             (*tail) = new_node;
@@ -21,11 +21,11 @@ void create_list(FILE *list_file, Node **head, Node **tail) // This function wil
         {
             Node *temp;
             temp = (*tail);
-            temp->next= new_node; // tail will point to new node.
+            temp->next = new_node;    // tail will point to new node.
             (*tail) = new_node;       // New node will become new tail.
-            new_node->next = (*head);     // Since, it is circular linked list tail will point to head.
+            new_node->next = (*head); // Since, it is circular linked list tail will point to head.
         }
         assert(((*tail)->next = (*head)) && ((*tail)->data = value)); // make sure its a circular linked list
-        create_list(list_file, head, tail);    // recursion
+        create_list(list_file, head, tail);                           // recursion
     }
 }
