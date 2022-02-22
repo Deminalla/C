314cd5bc22249abfb02d6040ae9f29ca1e688c4d
Deniza <65849358+Deminalla@users.noreply.github.com>
2022-02-22 20:51:44 +0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,8 +1,7 @@
+// DA
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-#define MAX 10 // if there is a limit in stack elements
 
 struct Node
 {
@@ -10,24 +9,25 @@ struct Node
     struct Node *next;
 };
 typedef struct Node Node;
-Node *top[100] = {0};
+Node *top[500] = {0};
 
-void push(int j, int stack[], int *status);
-void pop(int j, int stack[], int status);
-void peek(int j, int stack[], int status);
-void check(int j, int stack[], int choice2, int *status);
+void push(int j, int stack[], int status[]);
+void pop(int j, int stack[], int status[]);
+void display(int j, int stack[], int status[]);
+void check(int j, int stack[], int choice2, int status[], int max[]);
 void delete (int j, int stack[]);
 
 int main()
 {
     int choice, choice2, value;
-    int status = -3;
-    int i = 0, j;
-    int stack[100] = {0};
+    int status[500];
+    int i = 0, j, limit;
+    int stack[500] = {0};
+    int max[500] = {0};
 
     while (1){
         printf("\n*********** MENU *************\n");
-        printf("1. Create stack \n2. Push \n3. Pop \n4. Peek \n5. Delete stack\n6. Check if stack is empty/full \n7. Exit\n");
+        printf("1. Create stack \n2. Push \n3. Pop \n4. View \n5. Delete stack\n6. Check if stack is empty/full \n7. Exit\n");
         printf("Enter your choice: ");
         if (scanf("%d", &choice) == 1 && isspace(getchar())){
             if (choice == 1){
@@ -35,27 +35,29 @@ int main()
                 scanf("%d", &i);
                 if (stack[i] < 1){
                     stack[i] = 1;
-                    status=-3; //stack exists but we dont know if full/empty/eh
+                    printf("Enter element limit: ");
+                    scanf("%d", &limit); //how many elements do you want
+                    max[i] = limit;
                 }
                 else printf("Stack already exists");
             }
             else if (choice == 2){
                 printf("Which stack to push to: ");
                 scanf("%d", &j);
-                check(j, stack, 2, &status);
-                push(j, stack, &status);
+                check(j, stack, 2, status, max);
+                push(j, stack, status);
             }
             else if (choice == 3){
                 printf("Which stack to pop out of: ");
                 scanf("%d", &j);
-                check(j, stack, 1, &status);
+                check(j, stack, 1, status, max);
                 pop(j, stack, status);
             }
             else if (choice == 4){
-                printf("Which stack to peek at: ");
+                printf("Which stack to look at: ");
                 scanf("%d", &j);
-                check(j, stack, 1, &status);
-                peek(j, stack, status);
+                check(j, stack, 1, status, max);
+                display(j, stack, status);
             }
             else if (choice == 5){
                 printf("Which stack to delete: ");
@@ -70,18 +72,17 @@ int main()
             else if (choice == 6){
                 printf("Which stack to check: ");
                 scanf("%d", &j);
-                printf("1. Empty \n2. Full \n");
-                printf("Enter your choice: ");
+                printf("1. Empty \n2. Full \nEnter your choice: ");
                 scanf("%d", &choice2);
-                check(j, stack, choice2, &status);
-                if (status != -1){
+                check(j, stack, choice2, status, max);
+                if (status[j] != 0){
                 if (choice2 == 1){
-                    if (status!=0){
+                    if (status[j]==-1){
                         printf("Stack is not empty\n");
                     }}
                 else if (choice2 == 2){
-                    if (status!=2){
-                        printf("Stack is not full\n"); // if there is a limit in stack elements
+                    if (status[j]==-2){
+                        printf("Stack is not full\n"); 
                     }}
                 }}
             else if (choice == 7){
@@ -92,55 +93,83 @@ int main()
     }
     return 0;
 }
-void push(int j, int stack[], int *status){
-    if ((*status) == 0 || (*status) == 1){
+void push(int j, int stack[], int status[]){
+    if (status[j] != 0 && status[j] ==- 2){
     int value;
     Node *new_node;
     printf("Enter value: ");
     scanf("%d", &value);
     new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL){
+        printf("Memory could not be allocated\nFailed to push\n");
+    }
+    else {
     new_node->data = value;
     new_node->next = top[j];
     top[j] = new_node; //push to the top
     stack[j]++;
     }
+    }
 }
-void pop(int j, int stack[], int status){
-    if (status > 0){
+void pop(int j, int stack[], int status[]){
+    if (status[j] != 0 && status[j] == -1){
         printf("%d is out of stack\n", top[j]->data);
+        Node* temp;
+        temp = top[j];
         stack[j]--; //dec stack element nr
         top[j] = top[j]->next; //previous element is now top
+        free(temp);
     }
 }
-void peek(int j, int stack[], int status){
-    if (status > 0){
-        printf("%d is the top of stack", top[j]->data);
+void display(int j, int stack[], int status[]){
+    int choice2;
+    if (status[j] != 0 && status[j] == -1){
+    printf("1. Top \n2. Whole stack \nEnter your choice: ");
+    scanf("%d", &choice2);
+        if (choice2==1){
+        printf("%d is top of stack\n", top[j]->data);
+        }
+        else if (choice2==2){
+            Node* temp = top[j];
+            while (temp != NULL){
+            printf("%d\n", temp->data);
+            temp = temp->next;
+            }
+        }
     }
 }
-void check(int j, int stack[], int choice2, int *status){
+void check(int j, int stack[], int choice2, int status[], int max[]){
     if (stack[j] < 1){
         printf("Such stack doesn't exist\n");
-        (*status) = -1;
+        status[j] = 0;
     }
     else{
         if (choice2 == 1){
             if (top[j] == NULL){
                 printf("Stack is empty\n");
-                (*status) = 0;
+                status[j] = 1;
             }
+            else status[j] = -1; //not empty
         }
         else if (choice2 == 2){
-            if (stack[j] - 1 >= MAX){ // if there is a limit in stack elements
-                printf("Stack overflow\n");
-                (*status) = 2;
+            if (stack[j] - 1 >= max[j]){  //-1 since when we create its stack[i] = 1
+                printf("Stack is full\n");
+                status[j] = 2;
             }
-        }
-        if ((*status) == -3){ 
-            (*status) = 1; //not full and not empty
+            else status[j] = -2; //not full
         }
     }
+    printf("status: %d\n", status[j]); //delete it later
 }
 void delete (int j, int stack[]){
+    Node* temp = top[j];
+    Node* next = NULL;
+ 
+    while (temp != NULL){
+        next = temp->next;
+        free(temp);
+        temp = next;
+    }
     stack[j] = 0;
     top[j] = NULL;
 }
